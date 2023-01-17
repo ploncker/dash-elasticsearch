0920d54ec4ec509c41654586228d65ca4c0b6fc6
Ross <10663787+ploncker@users.noreply.github.com>
2023-01-18 09:41:23 +1100

    Update elasticUI_dash.py

--- elasticUI_dash.py
+++ elasticUI_dash.py
@@ -7,7 +7,7 @@ Created on Tue Feb 26 20:06:20 2019
 """
 import dash
 #import dash_core_components as dcc
-import dash_html_components as html
+from dash import html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 from elasticsearch import Elasticsearch
